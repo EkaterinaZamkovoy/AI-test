a21c38db7fdd75b82707828cf5190d11612df7ba
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-06-09 21:29:24 +0300

    feat: create RadioGroup

---
+++ src/entities/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,45 @@
+import { RadioButton } from '@/shared';
+import styles from './RadioGroup.module.scss';
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type Props = {
+  label?: string;
+  name: string;
+  value: string;
+  onChange: (value: string) => void;
+  options: Option[];
+  error?: string;
+  className?: string;
+};
+
+export const RadioGroup = ({
+  label,
+  name,
+  value,
+  onChange,
+  options,
+  error,
+}: Props) => {
+  return (
+    <div className={styles.group}>
+      {label && <p className={styles.groupLabel}>{label}</p>}
+      <div className={styles.options}>
+        {options.map((option) => (
+          <RadioButton
+            key={option.value}
+            name={name}
+            label={option.label}
+            value={option.value}
+            checked={value === option.value}
+            onChange={() => onChange(option.value)}
+          />
+        ))}
+      </div>
+      {error && <p className={styles.error}>{error}</p>}
+    </div>
+  );
+};
