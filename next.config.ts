ebfeaedee8ff83b8faa840270273848ba1b9f38a
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-06-10 17:12:49 +0300

    fix: update nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const path = require('path');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/assets/style')],
+    prependData: `@import '@/styles/variables.scss';`,
+  },
 };
 
 export default nextConfig;
